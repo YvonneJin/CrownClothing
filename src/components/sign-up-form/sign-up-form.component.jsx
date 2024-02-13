799fef0eed73fc7b480526434903240f7da41586
YvonneJin <11867229@qq.com>
2024-02-13 15:26:31 +1300

    Use onAuthStateChanged from firebase to observe user state

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+import { UserContext } from '../../context/user.context';
+
 import './sign-up-form.styles.scss';
 
 
@@ -19,6 +22,8 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
+    const { setCurrentUser } = useContext(UserContext)
+
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
@@ -33,6 +38,9 @@ const SignUpForm = () => {
 
         try {
             const { user } = await createAuthUserWithEmailAndPassword(email, password);
+            
+            setCurrentUser(user);
+
             await createUserDocumentFromAuth(user, { displayName });
             resetFormFields();
         } catch(error) {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,31 +3,41 @@ import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../context/user.context'; 
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
 
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
 
-    return (
-      <Fragment>
-        <div className='navigation'>
-            <Link className='logo-container' to='/'>
-                <CrwnLogo className='logo'/>
-            </Link>     
-            <div className='nav-links-container'>
-                <Link className='nav-link' to='/shop'>
-                    Shop
-                </Link>
-                <Link className='nav-link' to='/auth'>
-                    Sign In
-                </Link>
-            </div>
-        </div>
-        <Outlet />
-      </Fragment>
-    )
+  const signOutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null);
   }
+
+  return (
+    <Fragment>
+      <div className='navigation'>
+          <Link className='logo-container' to='/'>
+              <CrwnLogo className='logo'/>
+          </Link>     
+          <div className='nav-links-container'>
+              <Link className='nav-link' to='/shop'>
+                  Shop
+              </Link>
+              {
+                currentUser ? 
+                    (<span className='nav-link' onClick={signOutHandler}>Sign Out</span>)
+                    : (<Link className='nav-link' to='/auth'>
+                        Sign In
+                      </Link>)
+              }    
+          </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
 
   export default Navigation;

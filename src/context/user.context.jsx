b6f5098fe9256111e6552167c399464c2371ff64
YvonneJin <11867229@qq.com>
2024-02-23 15:44:15 +1300

    Set up store and created user reducer

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -16,11 +16,15 @@ export const UserContext = createContext({
 
 export const USER_ACTION_TYPES = {
     SET_CURRENT_USER: 'SET_CURRENT_USER'
-}
-
-const userReducer = (state, action) => {
+  };
+  
+  const INITIAL_STATE = {
+    currentUser: null
+  };
+  
+  const userReducer = (state, action) => {
     const {type, payload} = action;
-
+  
     switch(type){
         case USER_ACTION_TYPES.SET_CURRENT_USER:
             return {
@@ -28,13 +32,9 @@ const userReducer = (state, action) => {
                 currentUser: payload
             }
         default:
-            throw new Error(`Unhandled type ${type} in userReducer`);
+          throw new Error(`Unhandled type ${type} in userReducer`);
     }
-}
-
-const INITIAL_STATE = {
-    currentUser: null
-}
+  };
 
 export const UserProvider = ({ children }) => {
     
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,14 @@
 import { Fragment, useContext } from 'react'; 
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { UserContext } from '../../context/user.context'; 
 import { CartContext } from '../../context/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 import { 
   NavigationContainer,
@@ -18,7 +19,7 @@ import {
 
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
 
   return (
@@ -29,13 +30,13 @@ const Navigation = () => {
           </LogoContainer>     
           <NavLinksContainer>
               <NavLink to='/shop'>
-                  Shop
+                  SHOP
               </NavLink>
               {
                 currentUser 
-                  ? (<NavLink as='span' onClick={signOutUser}>Sign Out</NavLink>)
+                  ? (<NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>)
                   : (<NavLink to='/auth'>
-                       Sign In
+                       SIGN IN
                     </NavLink>)
               }
               <CartIcon />    

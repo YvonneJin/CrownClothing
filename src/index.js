b432be9ff71f9821d830b3877b2acecbd3589164
YvonneJin <11867229@qq.com>
2024-02-27 11:13:20 +1300

    Categories reducer and selector setting up finished

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import './index.scss';
 
 import App from './App';
-import { CategoriesProvider } from './context/categories.context'; 
 import { CartProvider } from './context/cart.context';
 import { store } from './store/store'; 
 
@@ -16,11 +15,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,7 +1,5 @@
 import { useParams } from 'react-router-dom'; 
-
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { CategoriesContext } from '../../context/categories.context'; 
+import { useState, useEffect, Fragment } from 'react';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
@@ -10,10 +8,13 @@ import {
   CategoryTitle
 } from './category.styles';
 
+import { useSelector } from 'react-redux';
+import { selectCategoriesMap } from '../../store/categories/category.selector';
+
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
